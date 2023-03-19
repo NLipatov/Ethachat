4d27a306fcf28022ecae29dd5846f87bd1974392
Nikita Lipatov <6stringsohei@gmail.com>
2023-03-19 22:17:00 +0300

    MessageDispatcherHandler and MessageDispatcherHubSubscriptionManager entities added

---
+++ Limp/Client/HubInteraction/Handlers/HubConnectionHandlers/Implementations/MessageDispatcherHub/MessageDispatcherHandler.cs
@@ -0,0 +1,69 @@
+﻿using ClientServerCommon.Models;
+using ClientServerCommon.Models.Message;
+using Limp.Client.HubInteraction.Handlers.Helpers;
+using Limp.Client.HubInteraction.Handlers.HubConnectionHandlers.Contract;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.JSInterop;
+
+namespace Limp.Client.HubInteraction.Handlers.HubConnectionHandlers.Implementations.MessageDispatcherHub
+{
+    public class MessageDispatcherHandler : IHubHandler<MessageDispatcherHandler>
+    {
+        private readonly NavigationManager _navigationManager;
+        private readonly IJSRuntime _jSRuntime;
+        private HubConnection? messageDispatcherHub;
+        public MessageDispatcherHandler(NavigationManager navigationManager,
+            IJSRuntime jSRuntime)
+        {
+            _navigationManager = navigationManager;
+            _jSRuntime = jSRuntime;
+        }
+        public async Task<HubConnection> ConnectAsync()
+        {
+            messageDispatcherHub = new HubConnectionBuilder()
+                .WithUrl(_navigationManager.ToAbsoluteUri("/messageDispatcherHub"))
+                .Build();
+
+            messageDispatcherHub.On<Message>("ReceiveMessage", message =>
+            {
+                MessageDispatcherHubSubscriptionManager.CallMessageReceived(message);
+            });
+
+            messageDispatcherHub.On<Guid>("MessageWasReceivedByRecepient", messageId =>
+            {
+                MessageDispatcherHubSubscriptionManager.CallMessageReceivedByRecepient(messageId);
+            });
+
+            messageDispatcherHub.On<string, string>("ReceivePublicKey", (partnersUsername, partnersPublicKey) =>
+            {
+                MessageDispatcherHubSubscriptionManager.CallReceivePublicKey(partnersUsername, partnersPublicKey);
+            });
+
+            messageDispatcherHub.On<string>("OnMyNameResolve", username =>
+            {
+                MessageDispatcherHubSubscriptionManager.CallMyNameResolved(username);
+            });
+
+            messageDispatcherHub.On<List<UserConnections>>("ReceiveOnlineUsers", updatedTrackedUserConnections =>
+            {
+                MessageDispatcherHubSubscriptionManager.CallOnlineUsersReceived(updatedTrackedUserConnections);
+            });
+
+            await messageDispatcherHub.StartAsync();
+
+            await messageDispatcherHub.SendAsync("SetUsername", await JWTHelper.GetAccessToken(_jSRuntime));
+
+            return messageDispatcherHub;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (messageDispatcherHub == null)
+                return;
+
+            await messageDispatcherHub.StopAsync();
+            await messageDispatcherHub.DisposeAsync();
+        }
+    }
+}
---
+++ Limp/Client/HubInteraction/Handlers/HubConnectionHandlers/Implementations/MessageDispatcherHub/MessageDispatcherHubSubscriptionManager.cs
@@ -0,0 +1,69 @@
+﻿using ClientServerCommon.Models;
+using ClientServerCommon.Models.Message;
+
+namespace Limp.Client.HubInteraction.Handlers.HubConnectionHandlers.Implementations.MessageDispatcherHub
+{
+    public class MessageDispatcherHubSubscriptionManager
+    {
+        public static List<Action<Message>> OnMessageReceived { get; set; } = new();
+        public static List<Action<Guid>> OnMessageReceivedByRecepient { get; set; } = new();
+        public static List<Action<string, string>> OnReceivePublicKey { get; set; } = new();
+        public static List<Action<string>> OnMyNameResolved { get; set; } = new();
+        public static List<Action<List<UserConnections>>> OnOnlineUsersReceived { get; set; } = new();
+        public static void SubscribeToOnMessageReceived(Action<Message> callback)
+        {
+            OnMessageReceived.Add(callback);
+        }
+        public static void CallMessageReceived(Message message)
+        {
+            foreach (var method in OnMessageReceived)
+            {
+                method(message);
+            }
+        }
+        public static void SubscribeToOnMessageReceivedByRecepient(Action<Guid> callback)
+        {
+            OnMessageReceivedByRecepient.Add(callback);
+        }
+        public static void CallMessageReceivedByRecepient(Guid messageId)
+        {
+            foreach (var method in OnMessageReceivedByRecepient)
+            {
+                method(messageId);
+            }
+        }
+        public static void SubscribeToOnReceivePublicKey(Action<string, string> callback)
+        {
+            OnReceivePublicKey.Add(callback);
+        }
+        public static void CallReceivePublicKey(string partnersUsername, string partnersPublicKey)
+        {
+            foreach (var method in OnReceivePublicKey)
+            {
+                method(partnersUsername, partnersPublicKey);
+            }
+        }
+        public static void SubscribeToOnMyNameResolved(Action<string> callback)
+        {
+            OnMyNameResolved.Add(callback);   
+        }
+        public static void CallMyNameResolved(string username)
+        {
+            foreach(var method in OnMyNameResolved)
+            {
+                method(username);
+            }
+        }
+        public static void SubscribeToOnlineUsersReceived(Action<List<UserConnections>> callback)
+        {
+            OnOnlineUsersReceived.Add(callback);
+        }
+        public static void CallOnlineUsersReceived(List<UserConnections> userConnections)
+        {
+            foreach (var method in OnOnlineUsersReceived)
+            {
+                method(userConnections);
+            }
+        }
+    }
+}
